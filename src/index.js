c5a62bb68b759afe124bb338ff40230b4632e3fe
Khaki01 <69197275+Khaki01@users.noreply.github.com>
2021-02-09 13:42:50 +0600

    add: update index with right code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
+
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length==0) return 0;
+
+  let minVal = 99999999;
+  for(let i = 0; i<array.length; ++i){
+    if(array[i]<minVal) minVal=array[i];
+  }
+  return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length==0) return 0;
+
+  let maxVal = 0;
+  for(let i = 0; i<array.length; ++i){
+    if(array[i]>maxVal) maxVal=array[i];
+  }
+  return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length==0) return 0;
+
+  let sumVal = 0;
+  for(let i = 0; i<array.length; ++i){
+    sumVal+=array[i];
+  }
+  return sumVal/array.length;
 }
+
